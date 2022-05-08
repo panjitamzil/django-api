0cd89f0b7fe15bd665329b800af0e66e6105b78d
Panji Tamzil <panji.tamzil@gmail.com>
2022-05-08 15:33:26 +0700

    Remove value of config

--- RestAPI/settings.py
+++ RestAPI/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'RestAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'users',
-        'USER': 'postgres',
-        'PASSWORD': 'testong',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
